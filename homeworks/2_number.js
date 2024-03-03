ef230309f5b1c9ee51e5ae061d488b011744ff28
heetolee <dlckdgml25@gmail.com>
2024-03-03 17:15:50 +0900

    Docs:pre-w02 complete

--- homeworks/2_number.js
+++ homeworks/2_number.js
@@ -11,7 +11,7 @@
 
       it("나머지 연산자", function () {
         const result = 10 % 2;
-        const guess = 1;
+        const guess = 0;
 
         expect(result).to.equal(guess);
       });
@@ -19,7 +19,7 @@
       it("나머지 연산자, 대입 연산자, 후위 증감 연산자", function () {
         let a = 10 % 3;
         const result = a++;
-        const guess = 바코;
+        const guess = 1;
 
         expect(result).to.equal(guess);
       });
@@ -27,7 +27,7 @@
       it("나머지 연산자, 대입 연산자, 전위 증감 연산자", function () {
         let a = 10 % 3;
         const result = ++a;
-        const guess = 바코;
+        const guess = 2;
 
         expect(result).to.equal(guess);
       });
@@ -37,7 +37,7 @@
         a -= 5;
 
         const result = ++a;
-        const guess = 바코;
+        const guess = 6;
 
         expect(result).to.equal(guess);
       });
@@ -47,7 +47,7 @@
         a = a / 5;
 
         const result = a > 2;
-        const guess = 바코;
+        const guess = false;
 
         expect(result).to.equal(guess);
       });
@@ -55,7 +55,7 @@
       it("나머지 연산자, 대입 연산자, 숫자 변환", function () {
         const a = "2";
         const result = Number(a);
-        const guess = 바코;
+        const guess = 2;
 
         expect(result).to.equal(guess);
       });
@@ -63,7 +63,7 @@
       it("NOT 논리 연산자", function () {
         const a = 45;
         const result = !a;
-        const guess = 바코;
+        const guess = !45;
 
         expect(result).to.equal(guess);
       });
@@ -71,7 +71,7 @@
       it("NOT 논리 연산자", function () {
         const a = 45;
         const result = !!a;
-        const guess = 바코;
+        const guess = true;
 
         expect(result).to.equal(guess);
       });
@@ -79,7 +79,7 @@
       it("NOT 논리 연산자", function () {
         const a = 0;
         const result = !!a;
-        const guess = 바코;
+        const guess = false;
 
         expect(result).to.equal(guess);
       });
@@ -87,42 +87,42 @@
       it("NOT 논리 연산자", function () {
         const a = -0;
         const result = !!a;
-        const guess = 바코;
+        const guess = false;
 
         expect(result).to.equal(guess);
       });
 
       it("&& 논리 연산자", function () {
         const result = 30 && 0;
-        const guess = 바코;
+        const guess = 0;
 
         expect(result).to.equal(guess);
       });
 
       it("&& 논리 연산자", function () {
         const result = 0 && 10;
-        const guess = 바코;
+        const guess = 0;
 
         expect(result).to.equal(guess);
       });
 
       it("|| 논리 연산자", function () {
         const result = 7 || 3;
-        const guess = 바코;
+        const guess = 7;
 
         expect(result).to.equal(guess);
       });
 
       it("|| 논리 연산자", function () {
         const result = NaN || 3;
-        const guess = 바코;
+        const guess = 3;
 
         expect(result).to.equal(guess);
       });
 
       it("||, && 논리 연산자", function () {
         const result = 7 || (3 && 10);
-        const guess = 바코;
+        const guess = 7;
 
         expect(result).to.equal(guess);
       });
